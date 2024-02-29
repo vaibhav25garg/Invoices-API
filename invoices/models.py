b680fb253aa4f2048af7e5d5640e518617b6f4ff
Vaibhav <vaibhavgarg977@gmail.com>
2024-03-01 01:04:21 +0530

    first commit

---
+++ invoices/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Invoice(models.Model):
+    date = models.DateField()
+    customer_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Invoice {self.pk}"
+
+
+class InvoiceDetail(models.Model):
+    invoice = models.ForeignKey(Invoice, related_name='details', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Detail {self.pk} for Invoice {self.invoice.pk}"
